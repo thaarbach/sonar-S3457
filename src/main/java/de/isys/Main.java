be006bcae58fc94c65736ab1ab2981597ca164e4
Thomas Haarbach <thomas.haarbach@isys.de>
2024-02-02 15:13:30 +0100

    enabled logging

--- src/main/java/de/isys/Main.java
+++ src/main/java/de/isys/Main.java
@@ -1,5 +1,8 @@
 package de.isys;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Thomas Haarbach
@@ -10,15 +13,15 @@ package de.isys;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+	static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests fixing it.
-		System.out.printf("Hello and welcome!");
+		LOGGER.atInfo().log("Hello world");
 
 		for (int i = 1; i <= 5; i++) {
-			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-			System.out.println("i = " + i);
+			LOGGER.info("i = {}", i);
 		}
+
+
 	}
 }
