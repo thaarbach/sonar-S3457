bd7aafff99ecfb3af58b1da44c752c3c4664e786
Thomas Haarbach <thomas.haarbach@isys.de>
2024-02-02 15:14:26 +0100

    removed comments and example readme

--- src/main/java/de/isys/Main.java
+++ src/main/java/de/isys/Main.java
@@ -3,15 +3,6 @@ package de.isys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Thomas Haarbach
- * Date: 02.02.24
- * Time: 14:32
- * Copyright 2024 iSYS-Software GmbH Munich
- */
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
